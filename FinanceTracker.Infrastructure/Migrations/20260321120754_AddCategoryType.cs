d7cccd05ee3cff4b2755f9aa0f4b2f66173f7a88
Daniel <dktop126@list.ru>
2026-03-23 12:35:39 +0300

    Добавлен первый тест

---
+++ FinanceTracker.Infrastructure/Migrations/20260321120754_AddCategoryType.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FinanceTracker.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCategoryType : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Type",
+                table: "Categories",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Accounts",
+                type: "TEXT",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "TEXT");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Type",
+                table: "Categories");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Accounts",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "TEXT",
+                oldNullable: true);
+        }
+    }
+}
---
+++ tests/FinanceTracker.UnitTests/FinanceServiceTests.cs
@@ -0,0 +1,37 @@
+﻿using FinanceTracker.Application.Services;
+using FinanceTracker.Domain.Entities;
+using FinanceTracker.Domain.Enums;
+using FinanceTracker.Domain.Interfaces;
+using Xunit.v3;
+using Moq;
+
+namespace FinanceTracker.UnitTests;
+
+public class FinanceServiceTests
+{
+    [Fact]
+    public async Task DeleteTransaction_ShouldIncreaseBalance_WhenTransactionIsExpense()
+    {
+        var accountRepoMock = new Mock<IAccountRepository>();
+        var transRepoMock = new Mock<ITransactionRepository>();
+        var categoryRepoMock = new Mock<ICategoryRepository>();
+
+        var account = new Account { Id = Guid.NewGuid(), Balance = 1000 };
+        var transaction = new Transaction
+        {
+            Id = Guid.NewGuid(),
+            Amount = 200,
+            Type = TransactionType.Expense,
+            AccountId = account.Id
+        };
+        
+        transRepoMock.Setup(r => r.GetByIdAsync(transaction.Id)).ReturnsAsync(transaction);
+        accountRepoMock.Setup(r => r.GetByIdAsync(account.Id)).ReturnsAsync(account);
+        
+        var service = new FinanceService(transRepoMock.Object, categoryRepoMock.Object, accountRepoMock.Object);
+        
+        await service.DeleteTransactionAsync(transaction.Id);
+
+        Assert.Equal(1200, account.Balance);
+    }
+}

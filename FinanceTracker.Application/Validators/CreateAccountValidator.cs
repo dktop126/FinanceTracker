6e1b239358eade02a38ff65362a788119cb8a625
Daniel <dktop126@list.ru>
2026-05-06 15:54:53 +0300

    Обновлен валидатор создания счета

--- FinanceTracker.Application/Validators/CreateAccountValidator.cs
+++ FinanceTracker.Application/Validators/CreateAccountValidator.cs
@@ -7,6 +7,9 @@ public class CreateAccountValidator : AbstractValidator<CreateAccountDto>
 {
     public CreateAccountValidator()
     {
-        RuleFor(x => x.Name).NotNull().WithMessage("Укажите название счета.");
+        RuleFor(x => x.Name)
+            .NotNull().WithMessage("Укажите название счета.")
+            .NotEmpty().WithMessage("Название счета не может быть пустым.")
+            .MaximumLength(100).WithMessage("Название счета не может превышать 100 символов.");
     }
 }
